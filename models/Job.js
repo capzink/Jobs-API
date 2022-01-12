967160b18d0a8fd76202763a2af175605b02a030
Carlos <capz2003@gmail.com>
2022-01-12 15:16:34 -0500

    Creates model for jobs

--- models/Job.js
+++ models/Job.js
@@ -1,1 +1,27 @@
-require
+const mongoose = require('mongoose')
+
+const jobsSchema = new mongoose.Schema({
+    company:{
+        type:String,
+        required:[true, 'Please provide compnay name'],
+        maxlength: 50
+    },
+position:{
+    type:String,
+    required:[true, 'Please provide a company'],
+    maxlength: 100
+},
+status:{
+    type:String,
+    enum:['interview', 'declined', 'pending'],
+    default: 'pending'
+},
+createdBy:{
+    type:mongoose.Types.ObjectId,
+    ref:'User',
+    required:[true , 'Please provide user']
+}
+
+},{timestamps:true})
+
+module.exports = mongoose.model("Jobs", jobsSchema);
