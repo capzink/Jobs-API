c3d281b965a0227d6f95e585d41b9260ce5ad43a
Carlos <capz2003@gmail.com>
2022-01-12 14:12:22 -0500

    Adds Login checks

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const User = require('../models/User')
 const {StatusCodes} = require('http-status-codes')
-const {BadRequestError} = require ('../errors')
+const {BadRequestError, UnauthenticatedError} = require ('../errors')
 
 
 const register = async (req,res)=>{
@@ -14,7 +14,19 @@ const register = async (req,res)=>{
 }
 
 const login = async (req, res) => {
-    res.send("user login");
+    const {email, password} = req.body
+    if(!email || !password){
+        throw new BadRequestError('Please provide email, name, password')
+    }
+
+    const user = await User.findOne({email})
+    //compare password
+    if(!user){
+        throw new UnauthenticatedError('invalid credentials')
+    }
+    const token =user.createJWT()
+    res.status(StatusCodes.OK).json({user:{name:user.name},token})
+    
 };
 
 module.exports = {
