f6d6ddddfecdbcd404157947cac1afae8b44ba61
Carlos <capz2003@gmail.com>
2022-01-12 11:53:18 -0500

    Tests register route

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,11 @@
+const User = require('../models/User')
+
 const register = async (req,res)=>{
-    try {
-        res.send('register user')
-        
-    } catch (error) {
-        res.status(400).send('cant resgiter user')
-        
-    }
+   res.send("register user");
 }
 
 const login = async (req, res) => {
-  try {
     res.send("user login");
-  } catch (error) {
-    res.status(400).send("cant login");
-  }
 };
 
 module.exports = {
